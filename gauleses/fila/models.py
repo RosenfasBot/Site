f6c33d23e8b17d69aebdcca02247f03b3b4e7d7e
RosenfasBot <renanmat84@gmail.com>
2024-02-21 03:57:10 -0300

    Fix fila procedure

--- gauleses/fila/models.py
+++ gauleses/fila/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from pistas.models import Pista
 from gerencial.models import SingletonModel
 from django.contrib.postgres.fields import ArrayField
-
+ 
 class Fila(SingletonModel):
     # Fila é uma lista de ints, com os números das pistas
 
@@ -14,7 +14,8 @@ class Fila(SingletonModel):
 
     @property
     def pistas_livres(self):
-        return Pista.objects.filter(caminho__isnull=True).order_by(*self.ordem_fila)
+        return Pista.objects.order_by(*self.ordem_fila)
+        #return Pista.objects.filter(caminho__isnull=True).order_by(*self.ordem_fila)
 
     @property
     def all_pistas(self):
@@ -25,6 +26,7 @@ class Fila(SingletonModel):
 
     def get_proxima_pista(self, caminho):        
         # prioridades de qual atribuir:
+        # 0. Atribuída ao caminho
         # 1. Caminho pode pegar, plantada
         # 2. Caminho pode pegar, nao plantada
         # 3. Caminho não pode pegar, plantada
@@ -32,22 +34,34 @@ class Fila(SingletonModel):
 
         # resumo da ordenação: CA pode pegar, plantada, posição na fila
         pistas_livres = self.pistas_livres
+        
+        print(type(caminho))
+        print(caminho)
 
-        # P1 plantadas e disponiveis pro CA
-        p1 = pistas_livres.filter(status='plantada', caminhos_possiveis__in=[caminho], usuarios_possiveis__in=[caminho.CA_ativo]).first()
-        pista = p1
+
+        # P0 plantadas, no caminho do CA
+        p0 = pistas_livres.filter(status='plantada',  caminho=caminho).first()
+        pista = p0
         if not pista:
-            # P2 não plantadas e disponiveis 
-            p2 = pistas_livres.filter(caminhos_possiveis__in=[caminho], usuarios_possiveis__in=[caminho.CA_ativo]).first()
-            pista = p2
-            if not pista:             
-                # P3 plantada e não disponivel
-                p3 = pistas_livres.filter(status='plantada').first()
-                pista = p3
-                if not pista:             
-                    # P4 plantada e não disponivel
-                    p4 = pistas_livres.first()
-                    pista = p4
+            print("----------------------not p0----------------------")
+        # P1 plantadas e disponiveis pro CA
+            p1 = pistas_livres.filter(status='plantada', caminho__isnull=True, caminhos_possiveis__in=[caminho], usuarios_possiveis__in=[caminho.CA_ativo]).first()
+            pista = p1
+            if not pista:
+                print("----------------------not p1----------------------")
+                # P2 não plantadas e disponiveis 
+                p2 = pistas_livres.filter(caminhos_possiveis__in=[caminho], usuarios_possiveis__in=[caminho.CA_ativo]).first()
+                pista = p2
+                if not pista: 
+                    print("----------------------not p2----------------------")            
+                    # P3 plantada e não disponivel
+                    p3 = pistas_livres.filter(status='plantada').first()
+                    pista = p3
+                    if not pista:
+                        print("----------------------not p3---------------------------------")             
+                        # P4 plantada e não disponivel
+                        p4 = pistas_livres.first()
+                        pista = p4
         return pista
 
     def manda_pro_fim(self, pista_numero):
